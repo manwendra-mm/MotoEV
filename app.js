8cece9fb75de9fb42a6280cb7b91418b36caa481
Vivek <vivek66anand66@gmail.com>
2023-12-08 02:57:59 +0530

    login and signup page added

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
-
 const app =  express();
-
 const port = 3000;
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
 
 app.get("/", (req,res) => {
     res.render("index.ejs");
+})
+
+app.get("/login", (req, res)=>{
+    res.render("login.ejs")
+})
+app.get("/signup", (req, res)=>{
+    res.render("signup.ejs")
 })
 
 app.listen(port, () => {
